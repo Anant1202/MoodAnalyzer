efd70c948c1b2a97b6f7734909092ec33ca368b1
Anant1202 <anantsingh1202@gmail.com>
2022-06-04 16:23:44 +0530

    Welcome to Mood Analyzer Program

---
+++ MoodAnalyzer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer Program");
+        }
+    }
+}
+
